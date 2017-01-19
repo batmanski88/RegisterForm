0e97e8f32de012d2ba7a46c6c2513b19633f142c
batmanski88 <bacior1988@gmail.com>
2017-01-19 20:19:24 +0100

    Add files via upload

---
+++ RegistrationController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zadanko.Models;
+using Zadanko.Repository.IRepo;
+
+namespace Zadanko.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private IClientRepo _repo;
+
+        public RegistrationController(IClientRepo repo)
+        {
+            _repo = repo;
+        }
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RegisterForm()
+        {
+            var model = new RegistrationFormViewModel();
+            model.AdditionalInterests = new List<string>();
+            return View(model);
+        }
+
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+       public ActionResult RegisterForm(RegistrationFormViewModel model, string[] interest)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                var Client = new Client()
+                {
+                    FName = model.FName,
+                    LName = model.LName,
+                    Email = model.Email,
+                    Password = model.Password,
+                    Interests = string.Join(",", model.Interests.Select(s => s.ToString())),
+                    Receipt = model.Receipt
+
+                };
+
+                for (int i = 0; i < interest.Length; i++)
+                {
+                    AdditionalInterest inter = new AdditionalInterest();
+                    if (interest[i] != null)
+                    {
+                        inter.Interest = interest[i];
+                    }
+
+                    Client.AdditionalInterests.Add(inter);
+                }
+
+                _repo.RegisterClient(Client);
+                _repo.SaveChanges();
+
+                ModelState.Clear();
+                ViewBag.Message = "Zarejestrowano pomyślnie";
+                return View();
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        private string CheckFileSize(byte[] file)
+        {
+            int maxFileSize = 2;
+
+            if(file.Length > maxFileSize * 1024)
+            
+              return  ViewBag.File = "Rozmiar pliku jest za duży!";
+
+            return null;
+        }
+    }
+}
