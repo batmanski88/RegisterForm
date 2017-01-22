07207e5a184f75dfb616413b0b53638186afa781
batmanski88 <bacior1988@gmail.com>
2017-01-22 14:44:38 +0100

    Add files via upload

--- Zadanko/Repository/Repo/ClientRepo.cs
+++ Zadanko/Repository/Repo/ClientRepo.cs
@@ -16,9 +16,33 @@ namespace Zadanko.Repository.Repo
             _db = db;
         }
 
-        public void RegisterClient(Client client)
+        public void RegisterClient(RegistrationFormViewModel model, string[] interest)
         {
-            _db.Clients.Add(client);
+            var Client = new Client()
+            {
+                FName = model.FName,
+                LName = model.LName,
+                Email = model.Email,
+                Password = model.Password,
+                Interests = string.Join(",", model.Interests.Select(s => s.ToString())),
+                Receipt = model.Receipt
+
+            };
+
+            for (int i = 0; i < interest.Length; i++)
+            {
+                
+                if (interest[i] != "")
+                {
+                    AdditionalInterest inter = new AdditionalInterest();
+                    inter.Interest = interest[i];
+                    Client.AdditionalInterests.Add(inter);
+                }
+
+                
+            }
+            _db.Clients.Add(Client);
+           
         }
 
         public void SaveChanges()
